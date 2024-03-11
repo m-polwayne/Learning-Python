f701617fc2708f4da367bfb6ddcf078f2acd3d55
m-polwayne <137073033+m-polwayne@users.noreply.github.com>
2024-03-11 16:11:13 +0200

    final commit day1

--- basic dta structure practice.py
+++ basic dta structure practice.py
@@ -1,10 +1,27 @@
 #LISTS
-myList =[1,2,3,4]
+from operator import length_hint
+
+
+myList =[1,1,2,3,4,4]
 myList.append(5)
-print(myList)
+#print(myList)
 b=myList.copy()
 x=myList.pop()
 
-print(myList)
-print(x)
-print(b)
+#print(myList)
+#print(x)
+#print(b)
+
+
+##################################################
+
+#TUPLES AND SETS
+d='jjvbdygdudhghcx'
+mySet={"a",'b','c'}
+print(mySet)
+newSet=list(set(myList))#sets contain unique(no repetition)
+print("myList = " ,myList)
+print("set made from myList = " ,newSet)
+print(len(myList))
+print("d as a list",list(d))
+print("d as a set",set(d))
