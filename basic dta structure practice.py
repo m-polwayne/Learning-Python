15abe85c6dd9f1ef853622ae7c165cc3f16472e3
m-polwayne <137073033+m-polwayne@users.noreply.github.com>
2024-03-12 12:14:00 +0200

    first commit day 2

--- basic dta structure practice.py
+++ basic dta structure practice.py
@@ -8,16 +8,20 @@ myList.append(5)
 b=myList.copy()
 x=myList.pop()
 
-#print(myList)
-#print(x)
-#print(b)
+print(myList)
+print(x)
+print(b)
+
+print('##################################################')
+###################################################################
 
 
-##################################################
-
-#TUPLES AND SETS
+#SETS sets are random not ordered and sets are immutable, 
+#you cannot change a value in a set once declared but you 
+#can add and remove elements from a set
 d='jjvbdygdudhghcx'
 mySet={"a",'b','c'}
+mySet.update('646')
 print(mySet)
 newSet=list(set(myList))#sets contain unique(no repetition)
 print("myList = " ,myList)
@@ -25,3 +29,45 @@ print("set made from myList = " ,newSet)
 print(len(myList))
 print("d as a list",list(d))
 print("d as a set",set(d))
+j= list(d)
+
+v= set(d)
+v.discard('j')
+print(v)
+print(len(d))
+print(len(j))
+print(len(v)) 
+print('##################################################')
+#################################################################
+
+#TUPLES tuples are like lists but they are immutable and declared 
+#using tuple= ('t','u','p','l','e') and list = ['l','i','s','t']
+myTuple=('d', 'f', 'j')
+type(myTuple)
+u=myTuple[2]
+k='j'
+h='h'
+if 'd'not in myTuple:
+    print('yes')
+else: 
+    print('no')
+print(myTuple)
+print(type(myTuple))
+print('##################################################')
+#################################################################
+#DICTIONARIES dictionaries have key value pairs like a dictionary
+dictionary={"0":0, "1":1, "2":2, "3":3, "4":4,}
+
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print('original dictionary ', thisdict) 
+thisdict.update(name='Motshidisi')#adding to a dict using update
+print('dictionary after name update thisdict.update(name=Motshidisi)',thisdict) 
+thisdict['age']=26 #update using subscript
+print('dictionary after age update #thisdict[age]=26',thisdict) 
+#adding new key value pair to dictionary using update method
+thisdict.update({'surname':'Pholoane'})
+f1=dict(country ="south ahhh", province=' limpompo',)
+thisdict.update(f1)
+
+print('dictionary after adding new key value pair surname',thisdict) 
+print(thisdict.get('name'))
--- hexidecimalConverter.py
+++ hexidecimalConverter.py
@@ -1,6 +1,6 @@
 hexadecimalDict={
     "0":0, "1":1, "2":2, "3":3, "4":4, "5":5, "6":6, "7":7, 
-    "8":8,"9":9, "A":10, "B":11, "C":13, "D":14, "E":15
+    "8":8,"9":9, "A":10, "B":11, "C":13, "D":14, "E":15,
 }
 def convertHex(n):
     hex=[];
@@ -10,8 +10,10 @@ def convertHex(n):
     x= hexadecimalDict[hex[0]];
     y= hexadecimalDict[hex[1]];
     z= hexadecimalDict[hex[2]];
-   
-    dec= x*pow(16,2) + y*pow(16,1) + z*pow(16,0);
-    print(dec)
 
-convertHex("A21")
+    if x and y and z in hexadecimalDict.values():
+        print(x)
+        dec= x*pow(16,2) + y*pow(16,1) + z*pow(16,0);
+        print(dec)
+  
+convertHex("21B")
