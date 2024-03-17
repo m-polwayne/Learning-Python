7a9537aa7b3101d55bfb213f2c684dca11b0f5e4
m-polwayne <pholoanemotshidisi004@gmail.com>
2024-03-17 22:00:29 +0200

    sunday

--- learning python1.py
+++ learning python1.py
@@ -1,36 +1,37 @@
-#name=input(' enter name : ')
-#surname=input(' enter surname : ')
-#print("hello "+name.upper()+" "+surname.title())
-#print("what do you calll a bear with no teeth?\n\nA gummy bear")
-#print(len(name))
+# name=input(' enter name : ')
+# surname=input(' enter surname : ')
+# print("hello "+name.upper()+" "+surname.title())
+# print("what do you calll a bear with no teeth?\n\nA gummy bear")
+# print(len(name))
 
 
-#num1=input(' enter first number : ')
-#num2=input(' enter second number : ')
-#if num1<num2:
+# num1=input(' enter first number : ')
+# num2=input(' enter second number : ')
+# if num1<num2:
 #    print(num1+','+num2)
-#else:
-#    print(num2+','+num1) 
+# else:
+#    print(num2+','+num1)
 
 
-#colours=input(' enter your favorite color : ')
-#col=colours.lower()
-#if col=='red':
+# colours=input(' enter your favorite color : ')
+# col=colours.lower()
+# if col=='red':
 #   print('I like red too')
-#else:
+# else:
 #   print('I dont like '+col+", I like red")
 
-#text = ' This is some text. '
-#print(text.strip(' '))
+# text = ' This is some text. '
+# print(text.strip(' '))
 
 from math import sqrt
 import math
+from operator import index
 import random
 
 
-#print(round(6.990707,2))
-#print(sqrt(500))
-#print(round(math.pi,5))
+# print(round(6.990707,2))
+# print(sqrt(500))
+# print(round(math.pi,5))
 '''name=input('enter name: ')
 num= int(input('enter a number: '))
 if num >10:
@@ -39,5 +40,22 @@ if num >10:
 else:
     for i in range(0,num):   
         print(name)'''
-fruit_tuple = ('apple','banana','strawberry','orange')
-print(random.choice(fruit_tuple))
+'''fruit_tuple = ('apple','banana','strawberry','orange')
+print(random.choice(fruit_tuple))'''
+
+'''fruit_tuple = ('apple', 'banana', 'strawberry', 'orange')
+for t in fruit_tuple:
+    print(t)
+
+pos = int(input("enter which pfruit index: "))
+print(fruit_tuple[pos])
+print(fruit_tuple.index(fruit_tuple[pos]))
+l = ['f', 'g']
+g = l.index('g')
+del (l[g])
+print(l)'''
+
+stri = 'MyNameIsMotshidisi'
+stri = "".join([i if i.islower() else ' ' + i.lower()
+               if i in ["I", 'N'] else " "+i for i in stri])
+print(stri)
