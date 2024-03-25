5592430fb916165fb6a42e5de85b204ae7f23c48
m-polwayne <137073033+m-polwayne@users.noreply.github.com>
2024-03-25 15:52:24 +0200

    last cammmit day 1

--- dailydigest/getQuote.py
+++ dailydigest/getQuote.py
@@ -1,8 +1,12 @@
 import csv
+import json
 import random
+from urllib import request
+'''
+Retrieve a random quote from a specified csv file
+'''
 
-
-'''def getQuote(quotes_file='quotes.csv'):
+def getQuote(quotes_file='quotes.csv'):
     try:
         with open(quotes_file,'r') as csvfile:
             ccsvv= csv.reader(csvfile, delimiter='|')
@@ -10,9 +14,17 @@ import random
     except Exception as e:
         quotes=[{'author':'Eric Idle',
                  'quote':'Always look on the bright side of life'}]
-    print(random.choice((quotes))) 
-getQuote()'''
+    return random.choice(quotes)
+'''
+quote=getQuote(quotes_file=None)
+print(quote['quote']+' - '+quote['author'])'''
 
-with open('quotes.csv','r') as csvfile:
-    ccsvv= csv.reader(csvfile, delimiter='|')
-    quotes=[{'author':line[0],'quote':line[1] } for line in ccsvv]
+'''retrieve the current weather forecast from open weathermap'''
+
+def getWeather(coords={'lat':2, 'lon':43}):
+    try:
+        api_key='9286889f30eaefe86c4428f2cf4cb317'
+        url = 
+        data = json.load(request.urlopen(url))
+    except Exception as e:
+        pass
