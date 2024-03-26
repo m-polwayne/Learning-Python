f7fe6cd1935517cff08ae00a155b2c628152aeca
m-polwayne <pholoanemotshidisi004@gmail.com>
2024-03-26 19:41:39 +0200

    kghkj

--- python learnings/strings.py
+++ python learnings/strings.py
@@ -8,10 +8,11 @@ def lowerCase(text):
 def removepunctuation(text):
     punct = ['.', ',', ';', '-']
     for j in punctuation:
-        text = text.replace(j, '')
+        text = text.replace(j, '\n')
     return text
 
 
-text = 'kjjnsjjnjnjjuwuhi iudjjuj jdj mkm k ijiwndin'
-text = ' '.join([word for word in text.split() if len(word) <= 3])
-print(text)
+text = ' The clean text method is a static method because it does not belong to any particular class instance, whereas add text is an instance method that belongs to a particular instance of the class. Static variables like replace puncs can also be added to control which punctuations get replaced. Use either the class name or the class instance to refer to static variables, but cannot be done with instance methods.'
+# text = ' '.join([word for word in text.split() if len(word) <= 3])
+print(removepunctuation(text))
+print(lowerCase(text))
